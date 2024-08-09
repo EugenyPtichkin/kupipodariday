e4842f595be9e267c66f751b589705068017ff12
Eugeny Ptichkin <eugeny.ptichkin@novsu.ru>
2024-08-09 19:39:27 +0300

    заполнены схемы данных

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,40 @@
-export class Offer {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { IsUrl, IsNumber } from 'class-validator';
+import { Wish } from '../../wishes/entities/wish.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Offer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.offers)
+  user: User;
+
+  @ManyToOne(() => Wish, (wish) => wish.offers)
+  item: Wish;
+
+
+  @Column('decimal')
+  @IsNumber()
+  amount: number;
+
+
+
+  
+}
--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,47 @@
-export class Wishlist {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  ManyToMany,
+} from 'typeorm';
+import { Length, IsUrl } from 'class-validator';
+import { Wish } from '../../wishes/entities/wish.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class WishList {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @Length(0, 1500)
+  description: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @ManyToOne(() => Wish, (wishes) => wishes.name)
+  wishes: Wish[];
+
+  @ManyToOne(() => User, (users) => users.wishlists)
+  owner: User;
+
+  @ManyToMany(() => Wish, (wishes) => wishes.wishlist)
+  items: Wish[];
+}
